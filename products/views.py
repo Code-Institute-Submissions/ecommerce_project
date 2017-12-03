293cbea953c0d78250c8061879143acb67d449e3
Rory Comiskey <rorycomiskey@hotmail.com>
2017-12-03 19:53:47 +0000

    init commit

---
+++ products/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from .models import Product
+
+# Create your views here.
+def viewproducts(request):
+    products = Product.objects.all()
+    return render(request, 'viewproducts.html', {'products': products})
+    
+def do_search(request):
+    products = Product.objects.filter(name__icontains=request.GET['q'])
+    return render(request, 'viewproducts.html', {'products': products})
+    
+def get_index(request):
+    return render(request, 'index.html')
