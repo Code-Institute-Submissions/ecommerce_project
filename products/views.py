2facedc39e0f3c418a4049a8003bce60d3b5ac4b
Rory Comiskey <rorycomiskey@hotmail.com>
2017-12-07 11:46:35 +0000

    pagination

--- products/views.py
+++ products/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 from .models import Product
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
-# Create your views here.
 def viewproducts(request):
-    products = Product.objects.all()
-    return render(request, 'viewproducts.html', {'products': products})
+    products = Product.objects.get_queryset().order_by('id')
+    page = request.GET.get('page', 1)
+    paginator = Paginator(products, 8)
+    try:
+        products = paginator.page(page)
+    except PageNotAnInteger:
+        products = paginator.page(1)
+    except EmptyPage:
+        products = paginator.page(paginator.num_pages)
+    return render(request, 'viewproducts.html', {'products': products}) 
     
 def do_search(request):
     products = Product.objects.filter(name__icontains=request.GET['q'])
@@ -12,3 +20,11 @@ def do_search(request):
     
 def get_index(request):
     return render(request, 'index.html')
+    
+    
+ 
+    
+
+    
+    
+    
--- search/views.py
+++ search/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from products.models import Product
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def do_search(request):
     products = Product.objects.filter(product_name__icontains=request.GET['q'])
+    page = request.GET.get('page', 1)
+    paginator = Paginator(products, 8)
+    try:
+        products = paginator.page(page)
+    except PageNotAnInteger:
+        products = paginator.page(1)
+    except EmptyPage:
+        products = paginator.page(paginator.num_pages)
     return render(request, "viewproducts.html", {"products": products})
